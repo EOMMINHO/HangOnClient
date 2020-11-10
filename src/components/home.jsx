818762998ccd23eeef6a62aa42c41f44be1f60f2
EOMMINHO <djaalsgh159@kaist.ac.kr>
2020-11-10 21:36:31 +0900

    home changed

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,12 +4,14 @@ class HomePage extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Hang On</h1>
         <div>
-          <button className="button">Host</button>
+          <h1 className="is-size-1 has-text-black mt-6">Hang On</h1>
         </div>
         <div>
-          <button className="button">Join</button>
+          <button className="button is-size-5 mt-6 px-6">Host</button>
+        </div>
+        <div>
+          <button className="button is-size-5 mt-4 px-6">Join</button>
         </div>
       </div>
     );
