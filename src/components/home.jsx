a80ed9a8b50fa719152408920ebb1ff653754680
EOMMINHO <djaalsgh159@kaist.ac.kr>
2020-11-10 22:44:13 +0900

    renewal

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,32 @@
 import React, { Component } from "react";
+import { io } from "socket.io-client";
 
 class HomePage extends Component {
+  socket;
+  constructor() {
+    super();
+    // Set connection
+    this.socket = io("http://localhost:5000");
+    // IO handler
+    this.socket.on("hostReady", (data) => {
+      console.log(data);
+    });
+    this.socket.on("newJoin", (playerName) => {
+      console.log(playerName);
+    });
+    // Binds
+    this.handleHostClick = this.handleHostClick.bind(this);
+    this.handleJoinClick = this.handleJoinClick.bind(this);
+  }
+
+  handleHostClick() {
+    this.socket.emit("host", "playerName");
+  }
+
+  handleJoinClick() {
+    this.socket.emit("join", "PlayerName", "room1");
+  }
+
   render() {
     return (
       <div className="container">
@@ -8,10 +34,20 @@ class HomePage extends Component {
           <h1 className="is-size-1 has-text-black mt-6">Hang On</h1>
         </div>
         <div>
-          <button className="button is-size-5 mt-6 px-6">Host</button>
+          <button
+            className="button is-size-5 mt-6 px-6"
+            onClick={this.handleHostClick}
+          >
+            Host
+          </button>
         </div>
         <div>
-          <button className="button is-size-5 mt-4 px-6">Join</button>
+          <button
+            className="button is-size-5 mt-4 px-6"
+            onClick={this.handleJoinClick}
+          >
+            Join
+          </button>
         </div>
       </div>
     );
