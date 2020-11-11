000bda8da913fee976923036474bf79d6f73ddb5
unknown <psgun79@kaist.ac.kr>
2020-11-11 20:48:31 +0900

    11/11 8PM

--- src/client.js
+++ src/client.js
@@ -1,7 +1,19 @@
 // DOM elements.
 const roomSelectionContainer = document.getElementById('room-selection-container')
 const roomInput = document.getElementById('room-input')
+const nameInput = document.getElementById('name-input')
 const connectButton = document.getElementById('connect-button')
+
+const clinkButton = document.getElementById('clink-button')
+const clinkYesButton = document.getElementById('clink-yes-button')
+const clinkNoButton = document.getElementById('clink-no-button')
+
+const gameListButton = document.getElementById('game-list-button')
+const gameYesButton = document.getElementById('game-yes-button')
+const gameNoButton = document.getElementById('game-no-button')
+
+const attentionButton = document.getElementById('attention-button')
+const stopAttentionButton = document.getElementById('stop-attention-button')
 
 const videoChatContainer = document.getElementById('video-chat-container')
 const localVideoComponent = document.getElementById('local-video')
@@ -35,25 +47,90 @@ connectButton.addEventListener('click', () => {
   joinRoom(roomInput.value)
 })
 
+clinkButton.addEventListener('click', () => {
+  clinkRequest()
+})
+clinkYesButton.addEventListener('click', () => {
+  joinClink()
+})
+clinkNoButton.addEventListener('click', () => {
+  refuseClink()
+})
+
+gameListButton.addEventListener('click', () => {
+  gameList()
+})
+
+attentionButton.addEventListener('click', () => {
+  attentionRequest()
+})
+
 // SOCKET EVENT CALLBACKS =====================================================
-socket.on('room_created', async () => {
+socket.on('hostResponse', async () => {
   console.log('Socket event callback: room_created')
 
   await setLocalStream(mediaConstraints)
   isRoomCreator = true
 })
 
-socket.on('room_joined', async () => {
-  console.log('Socket event callback: room_joined')
-
-  await setLocalStream(mediaConstraints)
-  socket.emit('start_call', roomId)
+socket.on('joinResponse', async (success) => {
+  if (success) {
+    await setLocalStream(mediaConstraints)
+    console.log('Socket event callback: room_joined')
+    socket.emit('start_call', roomId)
+  } else {
+    console.log('Socket event callback: full_room')
+    alert('The room is full, please try another one')
+  }
 })
 
-socket.on('full_room', () => {
-  console.log('Socket event callback: full_room')
+socket.on('clinkResponse', (playerName) => {
+  console.log('clink requested by '+playerName)
+  if (playerName != nameInput.value) {
+    // 오버레이 띄움(clink 요청 안내, clinkYesButton, clinkNoButton)
+  } else {
+    // 오버레이 띄움(clink 시작 버튼)
+  }
+})
+socket.on('clinkFail', (playerName) => {
+  console.log('clink fail: '+playerName)
+  if (playerName == nameInput.value) alert('The clink feature is already in use')
+})
 
-  alert('The room is full, please try another one')
+socket.on('gameResponse', (playerName, gameName) => {
+  console.log('game "'+gameName+'" requested by '+playerName)
+  if (playerName != nameInput.value) {
+    // 오버레이 띄움(game 요청 안내, gameYesButton, gameNoButton)
+  } else {
+    // 오버레이 띄움(game 시작 버튼)
+  }
+})
+socket.on('gameFail', (playerName) => {
+  console.log('game fail: '+playerName)
+  if (playerName == nameInput.value) alert('The game feature is already in use')
+})
+
+socket.on('attentionResponse', (playerName) => {
+  console.log('attention requested by '+playerName)
+  if (playerName != nameInput.value) {
+    // playerName 이름의 사용자의 캠을 내 화면 정가운데에 확대해서 둠
+  } else {
+    // 오버레이 띄움(자신이 attention 사용 중임을 안내, stopAttentionButton)
+  }
+})
+socket.on('attentionFail', (playerName) => {
+  console.log('attention fail: '+playerName)
+  if (playerName == nameInput.value) alert('The attention feature is already in use')
+})
+
+socket.on('seatSwapResponse', (participants) => {
+  console.log('seat swap occurred')
+  // participants 리스트대로 좌석배치도 다시 출력
+})
+
+socket.on('seatShuffleResponse', (participants) => {
+  console.log('seat shuffle occurred')
+  // participants 리스트대로 좌석배치도 다시 출력
 })
 
 // FUNCTIONS ==================================================================
@@ -62,7 +139,7 @@ function joinRoom(room) {
     alert('Please type a room ID')
   } else {
     roomId = room
-    socket.emit('join', room)
+    socket.emit('join', (nameInput.value, room))
     showVideoConference()
   }
 }
@@ -82,4 +159,24 @@ async function setLocalStream(mediaConstraints) {
 
   localStream = stream
   localVideoComponent.srcObject = stream
+}
+
+function clinkRequest() {
+  socket.emit('clink', (nameInput.value, roomId))
+}
+function joinClink() {
+  // 오버레이 닫고 화면에 clink 띄움
+  // 서버에 수락했다고 알림
+}
+function refuseClink() {
+  // 오버레이 닫음
+  // 서버에 거절했다고 알림
+}
+
+function gameList() {
+  // 화면에 이용 가능한 게임 목록 띄움
+}
+
+function attentionRequest() {
+  socket.emit('attention', (nameInput.value, roomId))
 }
