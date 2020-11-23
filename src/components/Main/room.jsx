d5d49658ab0b2956b5d7f3c3d19305139507016f
unknown <psgun79@kaist.ac.kr>
2020-11-24 00:38:35 +0900

    youtube sharing restored

--- src/components/Main/room.jsx
+++ src/components/Main/room.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactPlayer from "react-player/youtube";
 import { io } from "socket.io-client";
 import ButtonDropdown from "./ButtonDropdown";
 import Chat from "./Chat";
@@ -47,7 +48,6 @@ class Room extends Component {
     chatOpen: false,
     youtubeOpen: false,
     full_screen: false,
-    YoutubeInProgress: false,
     youtubeLink: "https://www.youtube.com/watch?v=UkSr9Lw5Gm8",
     youtubeLinkInput: null,
     items: [],
@@ -233,6 +233,9 @@ class Room extends Component {
     this.handleVideo = this.handleVideo.bind(this);
     this.handleAudio = this.handleAudio.bind(this);
     this.handleModalOutClick = this.handleModalOutClick.bind(this);
+    this.handleYoutubeVideo = this.handleYoutubeVideo.bind(this);
+    this.handleYoutubeLink = this.handleYoutubeLink.bind(this);
+    this.handleLinkInput = this.handleLinkInput.bind(this);
     this.toastIfVisible = this.toastIfVisible.bind(this);
     this.handleFullScreen = this.handleFullScreen.bind(this);
     this.handleChatClose = this.handleChatClose.bind(this);
@@ -283,6 +286,14 @@ class Room extends Component {
       this.state.roomName
     );
     this.setState({ modalActive: false, swapInProgress: false });
+  }
+
+  handleLinkInput(event) {
+    this.setState({ youtubeLinkInput: event.target.value });
+  }
+
+  handleYoutubeLink() {
+    this.socket.emit("youtube link", this.state.youtubeLinkInput, this.state.roomName);
   }
 
   handleModalOutClick() {
@@ -355,6 +366,37 @@ class Room extends Component {
         p.addStream(this.stream);
       });
     }
+  }
+
+  handleYoutubeVideo() {
+    this.setState({ youtubeOpen: !this.state.youtubeOpen });
+  }
+
+  getYoutubeVideo() {
+    if (this.state.youtubeOpen) var classname = "box"; else var classname = "is-invisible";
+    return (
+      <div className={classname}>
+        <div>
+          <ReactPlayer
+            url={this.state.youtubeLink}
+            controls={true}
+            width="320px"
+            height="180px"
+          />
+        </div>
+        <div>
+          <input
+            style={{width: 260, height: 20}}
+            className="input"
+            placeholder="Youtube Link"
+            onChange={this.handleLinkInput}
+          />
+          <button style={{width: 60, height: 20}} className="button has-text-centered" onClick={this.handleYoutubeLink}>
+            Share
+          </button>
+        </div>
+      </div>
+    );
   }
 
   handleFullScreen() {
@@ -697,7 +739,7 @@ class Room extends Component {
               />
               <Draggable>
                 <Youtube>
-                  <YoutubePlayer visible={this.state.youtubeOpen} />
+                  {this.getYoutubeVideo()}
                 </Youtube>
               </Draggable>
               <div>{this.settable()}</div>
@@ -782,9 +824,7 @@ class Room extends Component {
                   ? "button is-large is-black"
                   : "button is-large is-white"
               }
-              handler={() => {
-                this.setState({ youtubeOpen: !this.state.youtubeOpen });
-              }}
+              handler={this.handleYoutubeVideo}
               fontawesome="fab fa-youtube"
               description="Share Video"
             />
